9d5364dc395af81b1cd72176b83c4896677e111d
Ely Kaufman <Biwydakid50@gmail.com>
2015-01-19 00:39:10 -0700

    Fixed memory leaks
    instead of using 1-2 gigs of ram it now barely uses any! also should be
    faster processing times

--- src/com/team3019/VisionCode/YellowToteTracker.java
+++ src/com/team3019/VisionCode/YellowToteTracker.java
@@ -29,28 +29,37 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 
 public class YellowToteTracker{
-	public static Scalar Red,Blue,Green,Yellow,thresh_Lower,thresh_Higher,grey_Lower,grey_higher;
 	static NetworkTable table;
+	static Thread Capture;
+	static Thread Process;
+	static Thread Send;
+	//Color constants
+	public static final Scalar 
+		Red = new Scalar(0, 0, 255),
+		Blue = new Scalar(255, 0, 0),
+		Green = new Scalar(0, 255, 0),
+		Yellow = new Scalar(0, 255, 255),
+		//for tape
+		thresh_Lower = new Scalar(0,110,0),
+		thresh_Higher = new Scalar(255,255,134),
+		//for grey tote
+		grey_Lower = new Scalar(48,60,35),
+		grey_higher = new Scalar(81,84,54);
+	
+	static final boolean Process_Gray = false;
+	public static ArrayList<MatOfPoint> contours = new ArrayList<>();
+	public static Mat frame,grey,original;
+	static int counter = 0;
 	public static void main(String[] args) {
 		//required for openCV to work -call before any functions of oCV are used
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		Red = new Scalar(0, 0, 255);
-		Blue = new Scalar(255, 0, 0);
-		Green = new Scalar(0, 255, 0);
-		Yellow = new Scalar(0, 255, 255);
-		//for tape
-		thresh_Lower = new Scalar(62,102,0);
-		thresh_Higher = new Scalar(255,255,47);
-		//for grey tote
-		grey_Lower = new Scalar(48,60,35);
-		grey_higher = new Scalar(81,84,54);
-		
+		//initialize network tables
 		NetworkTable.setClientMode();
+		//the ip of the smartdashboard is "roborio-####.local" where #### is team number
 		NetworkTable.setIPAddress("roborio-3019.local");
 		table = NetworkTable.getTable("SmartDashboard");
 		
 		//main loop of the program
-		
 		while(true){
 			try {
 				while(table.isConnected()){
@@ -68,35 +77,35 @@ public class YellowToteTracker{
 		try {
 			//the url of the camera snapshot to save ##.## with your team number
 			//Url url = new URL("http://10.##.##.11/axis-cgi/jpg/image.cgi");
+			
+			//comment if testing a regular file
 			URL url = new URL("http://10.30.19.11/axis-cgi/jpg/image.cgi");
 			URLConnection uc = url.openConnection();
-			//saves the image to a file
-			BufferedImage img = ImageIO.read((uc.getInputStream()));
-			ImageIO.write(img, "png", new File("frame.png"));
+			BufferedImage image = ImageIO.read(uc.getInputStream());
+			ImageIO.write(image, "png", new File("frame.png"));
 			//time for the OpenCV fun!
-			Mat frame = new Mat();
-			Mat grey = new Mat();
-			Mat original = new Mat();
+			//frame = new Mat();
+			grey = new Mat();
 			original = Highgui.imread("frame.png");
-			ArrayList<MatOfPoint> contours = new ArrayList<>();
+			frame = original.clone();
+			//grey = original;
 			//applies a threshhold in the form of BlueGreenRed
 			Core.inRange(original, thresh_Lower, thresh_Higher, frame);
 			//find the cluster of particles
 			Imgproc.findContours(frame, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-			Imgproc.cvtColor(frame, frame, Imgproc.COLOR_GRAY2BGR);
 			//iterating through the list of contours and removing the ones with an "area" less then 100
 			for (Iterator<MatOfPoint> iterator = contours.iterator(); iterator.hasNext();) {
 				MatOfPoint matOfPoint = (MatOfPoint) iterator.next();
 				if(matOfPoint.width() * matOfPoint.height() < 20){
 					iterator.remove();
-				}	
+				}
 			}
 			//if theres only one contour dont do the silly math of the bounding rectangles
 			if(contours.size() == 1){
 					Rect rec1 = Imgproc.boundingRect(contours.get(0));
-					Core.rectangle(frame, rec1.tl(), rec1.br(), Yellow);
+					Core.rectangle(original, rec1.tl(), rec1.br(), Yellow);
 					String string = "TargetFound at X:" + (rec1.tl().x + rec1.br().x) / 2 + "Y:" + (rec1.tl().y + rec1.br().y) / 2;
-					Core.putText(frame, string, new Point(200,frame.size().height-10), Core.FONT_HERSHEY_PLAIN, 1, Red);
+					Core.putText(original, string, new Point(200,frame.size().height-10), Core.FONT_HERSHEY_PLAIN, 1, Red);
 			}
 			//i wonder if there is two totes...
 			else if(contours.size() == 2 || contours.size() == 4){
@@ -124,9 +133,10 @@ public class YellowToteTracker{
 					if(br.y < rec.br().y){
 						br.y = rec.br().y;
 					}
+					table.putBoolean("TargetVisible", true);
 				}
 				Rect bb1 = new Rect(tl, br);
-				Core.rectangle(frame, bb1.br(),bb1.tl(), Yellow);
+				Core.rectangle(original, bb1.br(),bb1.tl(), Yellow);
 				Rect bb2 = null;
 				//the 4 L will make 4 contours
 				//if there was 4 contours then fill the outer bounds of the second rectangle
@@ -148,16 +158,16 @@ public class YellowToteTracker{
 						}
 					}
 					bb2 = new Rect(tl, br);
+					table.putBoolean("TargetVisible", true);
 				}
 				String string;
 				if(null != bb2){
-					Core.rectangle(frame, bb2.br(),bb2.tl(), Yellow);
+					Core.rectangle(original, bb2.br(),bb2.tl(), Yellow);
 					//checking to see if the totes line up, because stacked totes line up!
 					if(Math.abs(bb1.x-bb2.x) < 5){
 						string = "Two totes stacked";
-						Core.line(frame, new Point(bb1.x+bb1.width/2,bb1.y+bb1.height/2),
+						Core.line(original, new Point(bb1.x+bb1.width/2,bb1.y+bb1.height/2),
 								new Point(bb2.x+bb2.width/2,bb2.y+bb2.height/2), Green);
-						//System.out.println(Math.abs((bb1.y + bb1.height/2) - (bb2.y + bb2.height)));
 					}
 					else{
 					string = "Two totes found";
@@ -165,21 +175,20 @@ public class YellowToteTracker{
 				}
 				else{
 					//knowing how far off the center of the image you can set your robot to align with it, essentially going towards it
-					string = "Off center by " + (bb1.x + bb1.width/2 - 320) + " pixels";
-					table.putNumber("Off Center", (bb1.x + bb1.width/2 - 320));
-					int print = bb1.width;
-					System.out.println(print / 640.0);
+					string = "Off center by " + (bb1.x + bb1.width/2 - 330) + " pixels";
+					table.putNumber("Off Center", (bb1.x + bb1.width/2 - 330));
 					table.putNumber("ToteToScreen", bb1.width / 640.0);
 				}
 				//putting the data in a visible form
-				Core.putText(frame, string, new Point(200,frame.size().height-10), Core.FONT_HERSHEY_PLAIN, 1, Red);
+				Core.putText(original, string, new Point(200,frame.size().height-10), Core.FONT_HERSHEY_PLAIN, 1, Red);
+				table.putBoolean("TargetVisible", true);
 			}
 			else if(contours.size() == 3){
 				ArrayList<Rect> rect = new ArrayList<>();
 				for(MatOfPoint mOP : contours){
 					rect.add(Imgproc.boundingRect(mOP));
 					Rect temp = rect.get(rect.size()-1);
-					Core.rectangle(frame, temp.br(),temp.tl(), Red);
+					Core.rectangle(original, temp.br(),temp.tl(), Red);
 				}
 				for(int i = 1; i < contours.size();i++){
 					if(Math.abs(rect.get(i).y - rect.get(i-1).y) < 5){
@@ -203,54 +212,51 @@ public class YellowToteTracker{
 						break;
 					}
 				}
+				table.putBoolean("TargetVisible", true);
 			}
 			else{
-				
+				table.putBoolean("TargetVisible", false);
 				for(MatOfPoint mOP : contours){
 					Rect rec = Imgproc.boundingRect(mOP);
-					Core.rectangle(frame, rec.tl(), rec.br(), Red);
+					Core.rectangle(original, rec.tl(), rec.br(), Red);
+				}
+				contours.clear();
+			}
+			if(Process_Gray){
+				//processing for grey totes
+				Core.inRange(grey, grey_Lower, grey_higher, grey);
+				Imgproc.findContours(grey, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+				Imgproc.cvtColor(grey, grey, Imgproc.COLOR_GRAY2BGR);
+				//iterating through the list of contours and removing the ones with an "area" less then 100
+				for (Iterator<MatOfPoint> iterator = contours.iterator(); iterator.hasNext();) {
+					MatOfPoint matOfPoint = (MatOfPoint) iterator.next();
+					if(matOfPoint.width() * matOfPoint.height() < 100){
+						iterator.remove();
+					}	
+				}
+				Rect bb;
+				for(MatOfPoint mop : contours){
+					bb = Imgproc.boundingRect(mop);
+					Core.rectangle(grey, bb.tl(), bb.br(), Red);
 				}
 			}
-			//processing for grey totes
-			Core.inRange(original, grey_Lower, grey_higher, grey);
-			Imgproc.findContours(grey, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-			Imgproc.cvtColor(grey, grey, Imgproc.COLOR_GRAY2BGR);
-			//iterating through the list of contours and removing the ones with an "area" less then 100
-			for (Iterator<MatOfPoint> iterator = contours.iterator(); iterator.hasNext();) {
-				MatOfPoint matOfPoint = (MatOfPoint) iterator.next();
-				if(matOfPoint.width() * matOfPoint.height() < 150){
-					iterator.remove();
-				}	
-			}
-			for(MatOfPoint mop : contours){
-				Rect bb = Imgproc.boundingRect(mop);
-				Core.rectangle(grey, bb.tl(), bb.br(), Red);
-			}
 			//gui on the image
-			Core.line(frame, new Point(frame.width()/2,100),new Point(frame.width()/2,frame.height()-100), Blue);
-			Core.line(frame, new Point(150,frame.height()/2),new Point(frame.width()-150,frame.height()/2), Blue);
-			Core.putText(frame, "Team 3019 - Elijah Kaufman", new Point(0,20), 
+			Core.line(original, new Point(frame.width()/2,100),new Point(frame.width()/2,frame.height()-100), Blue);
+			Core.line(original, new Point(150,frame.height()/2),new Point(frame.width()-150,frame.height()/2), Blue);
+			Core.putText(original, "Team 3019 - Elijah Kaufman", new Point(0,20), 
 					Core.FONT_HERSHEY_PLAIN, 1, Red);
 			//view this file to see the vision tracking
 			//windows will update the image after every save
-
-			Highgui.imwrite("rectangle.png", frame);
-			//Highgui.imwrite("grey.jpg", grey);
+			//pretty fast without it
+			Highgui.imwrite("rectangle.png", original);
+			//releases the mats from ram...frees up so much memory!
+			original.release();
+			grey.release();
+			frame.release();
 		//mostly for debugging but errors happen
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();	
 		}
-	}
-	//takes in the target width or length and a boolean and returns a distance
-	//assumes the use of the Axis M1013 camera
-	//depreciated because it needs further testing and does not work
-	@Deprecated
-	public static double getDistance(double targetInPixels,boolean width){
-		//double FOVpix = width ? 640 : 480;
-		double FOVpix = 480;
-		double FOVft = ((7 / 12.0) / targetInPixels) * FOVpix;
-		double returnd = FOVft / Math.tan(33.5);
-		return returnd;
 	}
 }
